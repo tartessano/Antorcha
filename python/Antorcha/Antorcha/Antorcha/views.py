4a6f1ac7fb973ce076038092aa91566134b2b011
Tarte <tarteferreras@gmail.com>
2018-12-28 15:02:10 +0100

    python update

--- python/Antorcha/Antorcha/Antorcha/views.py
+++ python/Antorcha/Antorcha/Antorcha/views.py
@@ -4,104 +4,32 @@ Routes and views for the flask application.
 
 from datetime import datetime
 
+from flask import request
 from flask import *
 
-
-db = MySQLdb.connect(host="localhost",    
-                     user="apiRest",        
-                     passwd="patata123",  
-                     db="Antorcha")      
-
-cur = db.cursor()
-
-
-@app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page, with a list of all polls."""
-    return render_template(
-        'index.html',
-        title='Polls',
-        year=datetime.now().year,
-        polls=repository.get_polls(),
-    )
-
-@app.route('/cliente', methods = ['POST'])
-def cliente():
-    cur.execute("SELECT * FROM Cliente")
-    return json.load()
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        repository_name=repository.name,
-    )
-
-@app.route('/seed', methods=['POST'])
-def seed():
-    """Seeds the database with sample polls."""
-    repository.add_sample_polls()
-    return redirect('/')
-
-@app.route('/results/<key>')
-def results(key):
-    """Renders the results page."""
-    poll = repository.get_poll(key)
-    poll.calculate_stats()
-    return render_template(
-        'results.html',
-        title='Results',
-        year=datetime.now().year,
-        poll=poll,
-    )
-
-@app.route('/poll/<key>', methods=['GET', 'POST'])
-def details(key):
-    """Renders the poll details page."""
-    error_message = ''
-    if request.method == 'POST':
-        try:
-            choice_key = request.form['choice']
-            repository.increment_vote(key, choice_key)
-            return redirect('/results/{0}'.format(key))
-        except KeyError:
-            error_message = 'Please make a selection.'
-
-    return render_template(
-        'details.html',
-        title='Poll',
-        year=datetime.now().year,
-        poll=repository.get_poll(key),
-        error_message=error_message,
-    )
-
-@app.errorhandler(PollNotFound)
-def page_not_found(error):
-    """Renders error page."""
-    return 'Poll does not exist.', 404
-
-
-
 import MySQLdb
-
+app = Flask(__name__)
 db = MySQLdb.connect(host="localhost",    
                      user="apiRest",        
                      passwd="patata123",  
                      db="Antorcha")      
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
 cur = db.cursor()
 
-# Use all the SQL you like
-cur.execute("SELECT * FROM Cliente")
+@app.route('/cliente', methods = ['GET'])
+def cliente():
+    cur = db.cursor()
+    cur.execute('''SELECT * FROM Cliente ''')
+    row_headers=[x[0] for x in cur.description] #this will extract row headers
+    rv = cur.fetchall()
+    json_data=[]
+    for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
+    return json.dumps(json_data)
 
-# print all the first cell of all the rows
-for row in cur.fetchall():
-    print row[0]
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
 
-db.close()
+
+
+
