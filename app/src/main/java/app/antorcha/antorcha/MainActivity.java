a542209a4392e009b35247ee93cce93f0bd5ccbb
POL3X <antoniogdlc1995@gmail.com>
2018-12-29 00:55:01 +0100

    3

--- app/src/main/java/app/antorcha/antorcha/MainActivity.java
+++ app/src/main/java/app/antorcha/antorcha/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import org.json.JSONArray;
@@ -24,36 +26,44 @@ public class MainActivity extends AppCompatActivity {
     private EditText user;
     private EditText pass;
 
+
     BaseDatos bbdd = new BaseDatos();
     LoginAPI loginAPI = new LoginAPI();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    //Inicializamos las dos entradas de texto user y pass
       user = (EditText) findViewById(R.id.User);
       pass = (EditText) findViewById(R.id.Pass);
+
 
     }
 
 
 
-    public void pulsar (View v){
+    public void pulsar (View v) throws ExecutionException, InterruptedException, JSONException {
+
+            //Capturamos las dos entradas de texto una vez pulsado el boton
+            String userS = user.getText().toString();
+            String passS = pass.getText().toString();
+
+            //Se la pasamos al servidor
+            String response = loginAPI.execute(userS + "/" + passS).get();
+
+            if (response == "null"){
+
+                Toast toast = Toast.makeText(getApplicationContext(), "Error Login", Toast.LENGTH_SHORT);
+                toast.show();
+            }
 
 
-
-
-
-
-             String response = loginAPI.execute("usuario/pass").get();
-             
-
-            /*String response = bbdd.execute("http://188.166.81.174:5000/cliente").get();
+            //String response = bbdd.execute("http://188.166.81.174:5000/cliente").get();
 
             JSONArray jsonArray = new JSONArray(response);
-            JSONObject jsonObject = jsonArray.getJSONObject(1);
-
-            String User = jsonObject.getString("User");*/
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+            
+           // String User = jsonObject.getString("User");
 
     }
 
