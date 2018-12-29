101979af1b45d94e8c6868373a7235a66dbdaf47
POL3X <antoniogdlc1995@gmail.com>
2018-12-29 02:34:04 +0100

    4

--- app/src/main/java/app/antorcha/antorcha/librerias/LoginAPI.java
+++ app/src/main/java/app/antorcha/antorcha/librerias/LoginAPI.java
@@ -2,55 +2,74 @@ package app.antorcha.antorcha.librerias;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.net.ssl.HttpsURLConnection;
 
-public class LoginAPI extends AsyncTask<String,String,String>{
+
+public class LoginAPI {
 
 
-    @Override
-    protected String doInBackground(String... params) {
+   /* protected String doInBackground(String... strings) {
 
         String line = "";
         try {
-
-            String parametros = params.toString();
+            Log.d("HASTA", "AQUI2");
+            //String parametros = strings.toString();
+           // Log.i("HASTA", parametros);
             String user , Pass;
-            String[] tokens = parametros.split("/");
-            user = tokens[0];
-            Pass = tokens[1];
+           // String[] tokens = parametros.split("/");
+           // user = tokens[0];
+            //Pass = tokens[1];
+            user = "pol";
+            Pass = "patata123";
+            Log.d("HASTA", "AQUI2");
             StringBuffer buffer = new StringBuffer();
-
+            Log.d("HASTA", "AQUIJDOER");
 
             String http="http://188.166.81.174:5000/login";
             URL url=new URL(http);
-
+            Log.d("HASTA", "AQUI");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setDoOutput(true);
-            con.setDoInput(true);
-            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            con.setRequestMethod("POST");
+            Log.d("HASTA", "AQUI");
+            con.connect();
+            int code = con.getResponseCode();
 
-            JSONObject cred = new JSONObject();
-            cred.put("User",user);
-            cred.put("Pass",Pass);
+            if (code == HttpURLConnection.HTTP_OK) {
+                con.setDoOutput(true);
+                con.setDoInput(true);
+                con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                con.setRequestMethod("POST");
 
-            OutputStream os = con.getOutputStream();
-            os.write(cred.toString().getBytes("UTF-8"));
-            os.close();
+                JSONObject cred = new JSONObject();
+                cred.put("User", user);
+                cred.put("Pass", Pass);
 
+                OutputStream os = con.getOutputStream();
+                os.write(cred.toString().getBytes("UTF-8"));
+                os.close();
+            }else{
 
-            InputStream in = new BufferedInputStream(con.getInputStream());
+                Log.d("ERROR", "CONECTAR");
+
+            }
+
+           /* InputStream in = new BufferedInputStream(con.getInputStream());
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
 
@@ -58,14 +77,14 @@ public class LoginAPI extends AsyncTask<String,String,String>{
                 buffer.append(line);
             }
 
-
+            in.close();
 
         }
         catch (Exception e){
             Log.v("ErrorAPP",e.toString());
         }
 
-        return line;
+        return "[]";
     }
 
     @Override
@@ -81,5 +100,60 @@ public class LoginAPI extends AsyncTask<String,String,String>{
     @Override
     protected void onProgressUpdate(String... values) {
         super.onProgressUpdate(values);
+    }*/
+    public static String sendPost() throws Exception {
+
+        String user , Pass;
+
+        user = "pol";
+        Pass = "patata123";
+
+    String url = "https://selfsolve.apple.com/wcResults.do";
+    URL obj = new URL(url);
+    HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+    //add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", "USER_AGENT");
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+
+
+    // Send post request
+		con.setDoOutput(true);
+    DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        JSONObject cred = new JSONObject();
+        cred.put("User", user);
+        cred.put("Pass", Pass);
+
+
+
+        wr.write(cred.toString().getBytes("UTF-8"));
+		wr.flush();
+		wr.close();
+
+
+
+    BufferedReader in = new BufferedReader(
+            new InputStreamReader(con.getInputStream()));
+    String inputLine;
+    StringBuffer response = new StringBuffer();
+
+    while ((inputLine = in.readLine()) != null) {
+        response.append(inputLine);
     }
+
+            in.close();
+
+
+    return response.toString();
 }
+
+
+
+}
+
+
+
+
+
